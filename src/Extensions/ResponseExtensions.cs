5f8ebefca3a068fbfe5d38eacbbcef96394a6b11
zaminalili <zaminalili29@gmail.com>
2025-04-30 22:35:21 +0400

    Add response extension method

---
+++ src/Extensions/ResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace youtube_indistract.Extensions;
+public static class ResponseExtensions
+{
+    public static async Task<T?> ToResponseAsync<T>(this HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            ReferenceHandler = ReferenceHandler.Preserve
+        });
+
+        return result;
+    }
+}
