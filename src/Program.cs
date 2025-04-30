07d88408c882eee56214b5446f02b7d7a3cba5b0
zaminalili <zaminalili29@gmail.com>
2025-04-30 22:37:16 +0400

    Add YoutubeApiService

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.googleapis.com/youtube/v3/") });
 
 builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+
+builder.Services.AddScoped<IYoutubeApiService, YoutubeApiService>();
 
 await builder.Build().RunAsync();
