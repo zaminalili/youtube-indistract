0143509a4a07badec093e270d94479f46e8298c1
zaminalili <zaminalili29@gmail.com>
2025-05-10 16:41:34 +0400

    Extract static method for MarkupString decoding

---
+++ src/Statics/Embed.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace youtube_indistract.Statics;
+
+public static class Embed
+{
+    public static MarkupString Decode(string videoId, int width = 560, int height = 315)
+    {
+        return new MarkupString($"<iframe width='{width}' height='{height}' src='https://www.youtube.com/embed/{videoId}?rel=0' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen> </iframe>");
+    }
+}
