f1728ddd3a8a73f3bf531ca7be616205c817f815
zaminalili <zaminalili29@gmail.com>
2025-05-10 18:15:58 +0400

    Add iframe embed responsive below 1050px

--- src/Statics/Embed.cs
+++ src/Statics/Embed.cs
@@ -6,6 +6,6 @@ public static class Embed
 {
     public static MarkupString Decode(string videoId, int width = 560, int height = 315)
     {
-        return new MarkupString($"<iframe width='{width}' height='{height}' src='https://www.youtube.com/embed/{videoId}?rel=0' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen> </iframe>");
+        return new MarkupString($"<iframe  src='https://www.youtube.com/embed/{videoId}?rel=0'  frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen> </iframe>");
     }
 }
