89ebd099312c9448a7ab94db60184a4c25f032f2
zaminalili <zaminalili29@gmail.com>
2025-05-31 11:34:48 +0400

    Add theme management script

---
+++ src/wwwroot/js/theme.js
@@ -0,0 +1,16 @@
+﻿function setTheme(theme) {
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+}
+
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+// Set initial theme
+setTheme(prefersDarkScheme.matches ? 'dark' : 'light');
+
+// Listen for changes
+prefersDarkScheme.addEventListener('change', (event) => {
+    setTheme(event.matches ? 'dark' : 'light');
+});
